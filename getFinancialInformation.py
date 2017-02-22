9d7050c0696bf2484da5ec54f2e89d183799f9e3
vishwaant <vishwaant.avk@gmail.com>
2017-02-22 00:41:48 -0500

    added classes for form count and 13f-hr parsing

--- getFinancialInformation.py
+++ getFinancialInformation.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import luigi
-import time,sys,json,os
-import datetime,httplib,urllib2
+import os
+import ujson
 import string,requests
 from bs4 import BeautifulSoup
-import pickle
-import struct
 
 output_folder = "../../../Data/SEC"
 lookups_folder = output_folder+"/lookups"
@@ -19,12 +17,15 @@ class PrepareEnv(luigi.Task):
         if not os.path.exists(lookups_folder): os.makedirs(lookups_folder)
 
     def output(self):
-        return luigi.LocalTarget(path=output_folder)
+        return [luigi.LocalTarget(path=output_folder),luigi.LocalTarget(path=lookups_folder)]
 
 
 class getCrawler(luigi.Task):
 
     date = luigi.DateParameter()
+
+    def requires(self):
+        return PrepareEnv()
 
     def run(self):
         CRAWLER_URL = URL+"crawler.idx"
@@ -36,9 +37,14 @@ class getCrawler(luigi.Task):
     def output(self):
         return luigi.LocalTarget(path=output_folder+"/crawler_{}.idx".format(self.date))
 
+
+
 class createCIKLookup(luigi.Task):
 
     date = luigi.DateParameter()
+
+    def requires(self):
+        return [PrepareEnv(),getCrawler(self.date)]
 
     def slices(self,s, *args):
         position = 0
@@ -48,18 +54,48 @@ class createCIKLookup(luigi.Task):
 
     def run(self):
         company_dict={}
-        print "++++++++++++"
+        forminfo = {}
         crawl_file =  open(output_folder + "/crawler_{}.idx".format(self.date), "r")
-        for line in crawl_file.readlines():
-            company,form,cik,rest = self.slices(line,62,12,12,98)
-            company_dict[cik]=company
-        # print company_dict
-        print "++++++++++++"
+
+        for line in crawl_file.readlines()[9:]:
+            company,form,cik,dateoffile,url = self.slices(line,62,12,12,12,86)
+            if cik is not "" : company_dict[cik.strip()]=company.strip()
+            if form.strip() not in forminfo:
+                forminfo[form.strip()]=1
+            else:
+                forminfo[form.strip()]=forminfo[form.strip()]+1
+
+        with open(output_folder+"/form_counts_{}.json".format(self.date),'w') as fm:
+            ujson.dump(forminfo,fm,indent=4)
+
+        with open(lookups_folder+"/company_{}.json".format(self.date),'w') as lkp:
+            ujson.dump(company_dict,lkp,indent=4)
+
+    def output(self):
+        return luigi.LocalTarget(path=lookups_folder+"/company_{}.json".format(self.date))
+
+
+class parse13FHR(luigi.Task):
+
+    date = luigi.DateParameter()
+    url = "https://www.sec.gov/Archives/edgar/data/1602119/0000950123-17-000678-index.htm"
+
+    def requires(self):
+        return [PrepareEnv(),getCrawler(self.date),createCIKLookup(self.date)]
+
+
+    def run(self):
+        sec_main_page = requests.get(self.url,timeout=60)
+        sec_soup = BeautifulSoup(sec_main_page.text,"lxml")
+        print [a["href"] for a in sec_soup.find_all('a') if a.text=="form13fInfoTable.html"]
+
 
     # def output(self):
-    #     return luigi.LocalTarget(path=output_folder + "/crawler_{}.idx".format(self.date))
+    #     return luigi.LocalTarget(path=lookups_folder+"/company_{}.json".format(self.date))
 
 
+
+    #     return luigi.LocalTarget(path=lookups_folder+"/company_{}.json".format(self.date))
 
 
 if __name__ == "__main__":
